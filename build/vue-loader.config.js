0db609d171e8652ee0de2b645a0929dfc4b0f706
lzcrwxl <lzcrwxl@163.com>
2018-03-23 13:50:11 +0800

    eslint

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,7 +3,10 @@ module.exports=(isDev)=>{
   return{
     preserveWhitepace:true,
     extractCSS:!isDev,
-    cssModules:{},
+    cssModules:{
+      localIdentName:isDev?'[path]-[name]-[hash:base64:5]':'[hash:base64:5]',
+      camelCase:true
+    },
     // hotReload:false //根据环境变量生成
   }
 }
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -39,7 +39,13 @@ if (isDev) {
           test: /\.styl/,
           use: [
             'vue-style-loader',
-            'css-loader',
+            {
+              loader:'css-loader',
+              // options:{
+              //   module:true,
+              //   localIdentName:isDev?'[path]-[name]-[hash:base64:5]':'[hash:base64:5]'
+              // }
+            },
             {
               loader: 'postcss-loader',
               options: {
